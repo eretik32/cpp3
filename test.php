e3f984b373eee89e3ca29f1281fe68b2aa6fcd83
pers1307 <skulines@mail.ru>
2019-07-17 10:05:11 +0500

    fix

--- test.php
+++ test.php
@@ -1,20 +1,26 @@
 <?php
 
 $ch = curl_init();
-//curl_setopt($ch, CURLOPT_URL, 'http://cpp/api/imageModeration/');// Загружаемый URL
-curl_setopt($ch, CURLOPT_URL, 'http://cpp.pindato.ru/api/imageModeration/');// Загружаемый URL
+curl_setopt($ch, CURLOPT_URL, 'http://cpp.local/api/imageModeration/');// Загружаемый URL
 curl_setopt($ch, CURLOPT_POST, 1); // указываем метод POST
 curl_setopt($ch, CURLOPT_TIMEOUT, 10); //время запроса
 curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
+
+$filePath = __DIR__ .'/lesTnica.jpg';
+
 curl_setopt($ch, CURLOPT_POSTFIELDS, [
-    'datafile' => curl_file_create(__DIR__ .'\lesTnica.jpg', 'image/jpg', 'hghff'),
+    'datafile' => curl_file_create($filePath, 'image/jpg', 'hghff'),
     'productId' => 1,
     'typeImage' => 1
 ]);
+
 // добавить тип картинки и id продукта
 $result = curl_exec($ch);
 if ($result === false) {
-  echo 'файл не найден';
+    $error = '';
+    $error = curl_error($ch);
+    echo $error;
+    echo curl_errno($ch);
 }
 
 curl_close($ch);
